2bd14906ce546f0131ea33783ae8a2c8582eeb46
Omargaly Bitebayev <omargaly.rs@gmail.com>
2022-03-15 07:03:57 +0600

    Создал класс PostModel для создания экземпляров данного класса в будущем, объявил приватные поля согласно условиям ДЗ. В классе использовал аннотации Data (для того, чтобы вручную не писать геттеры и сеттеры), AllArgsConstructor и NoArgsConstructor (для того, чтобы вручную не писать два конструктора: один из которых принимает параметры, а другой нет).

---
+++ src/main/java/kz/dar/academy/model/PostModel.java
@@ -0,0 +1,21 @@
+package kz.dar.academy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PostModel {
+    @NotNull(message = "Post id can not be empty")
+    private String postId;
+    @NotNull(message = "Client id can not be empty")
+    private String clientId;
+    @NotNull(message = "Recipient id can not be empty")
+    private String postRecipientId;
+    private String postItem;
+    private String status;
+}
